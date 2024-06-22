f68d52b2d5f7fe325c41845e2f056b50682d8e41
Andrew Snodgrass <pointlander@gmail.com>
2024-06-22 13:52:36 -0500

    Get counts per second

--- main.go
+++ main.go
@@ -4,6 +4,53 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"go.bug.st/serial"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	running := true
+	go func() {
+		<-c
+		running = false
+	}()
+
+	options := &serial.Mode{
+		BaudRate: 115200,
+	}
+	port, err := serial.Open("/dev/ttyUSB0", options)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = port.Write([]byte("<HEARTBEAT1>>"))
+	if err != nil {
+		panic(err)
+	}
+
+	for running {
+		data := make([]byte, 2)
+		_, err = port.Read(data)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println((uint64(data[0]) << 8) | uint64(data[1]))
+	}
+
+	_, err = port.Write([]byte("<HEARTBEAT0>>"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = port.Close()
+	if err != nil {
+		panic(err)
+	}
 }
